46932e7d6227e0f4ad51f4b9f9607867e5a1a383
lindalbertort7 <lindalberto.rufino@rt7.com.br>
2018-02-20 18:12:05 -0300

    feat: prosseguindo com a implementação do canvas.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,27 +16,21 @@ export class HomePage {
   public urlVideo: string;
   public videoPlayer: any;
   public c1: any;
-  public c2: any;
+  public ctx1: any;
 
   constructor(public navCtrl: NavController) {
-    this.urlVideo = 'https://videoaulas.infra.grancursosonline.com.br/564afe295161b94c3dcc76ab8ec071b3/eab5ebae49838bfc0226ada30231e6bc/eab5ebae49838bfc0226ada30231e6bc.mp4';
+    this.urlVideo = 'assets/video/VfE_html5.mp4';
   }
 
   ngOnInit() {
     this.videoPlayer = this.mVideoPlayer.nativeElement;
     this.c1 = this.mCanvas1.nativeElement;
-    this.c2 = this.mCanvas2.nativeElement;
-
-    let cxt1 = this.c1.getContext('2d');
-    let cxt2 = this.c2.getContext('2d');
+    //this.c1.width = this.videoPlayer.videoWidth;
+    //this.c1.height = this.videoPlayer.videoHeight;
+    this.ctx1 = this.c1.getContext('2d');
 
     this.videoPlayer.src = this.urlVideo;
-    let width, height;
     this.videoPlayer.onplay = () => {
-      console.log(this.videoPlayer.videoWidth);
-      width = this.videoPlayer.videoWidth / 2;
-      console.log(this.videoPlayer.videoHeight);
-      height = this.videoPlayer.videoHeight / 2;
       this.timerCallback();
     };
 
@@ -54,6 +48,19 @@ export class HomePage {
     setTimeout(function() {
       self.timerCallback();
     }, 0);
+  }
+
+  computeFrame() {
+    //this.ctx1.width = this.videoPlayer.videoWidth;
+    //this.ctx1.height = this.videoPlayer.videoHeight;
+    this.ctx1.drawImage(this.videoPlayer, 0, 0, (this.videoPlayer.videoWidth / 2), (this.videoPlayer.videoHeight /2));
+    return;
+  }
+
+  resize() {
+    console.log(this.videoPlayer);
+    this.c1.width = this.videoPlayer.videoWidth;
+    this.c1.height = this.videoPlayer.videoHeight;
   }
 
   changeSpeed(speed: number) {
